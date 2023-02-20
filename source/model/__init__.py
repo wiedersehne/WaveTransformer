ce20dea453eefdd702c7d2aac8d3a236b607e9f8
Charles <cwlgadd@gmail.com>
2023-02-20 16:47:49 +0000

    remove autoencoder models (with LSTM) and their enc/dec networks and callbacks

--- source/model/__init__.py
+++ source/model/__init__.py
@@ -1,5 +1,2 @@
 # Encoder networks
-from source.model.encoder.linear_encoder import LinearEncoder
 from source.model.encoder.sequence_encoder import SequenceEncoder
-# Decoder networks
-from source.model.decoder.linear import LinearDecoder
